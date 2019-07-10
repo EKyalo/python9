0bce8986f06a9522aa708802a584d0f8138017b4
Evans <evanomwendwa@gmail.com>
2019-07-10 23:56:34 +0300

    Update HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+def hello():
+	return ("Hello World")
+  
+    print("I will teach you to be rich")
+	
+#This being a python3 program is a very interesting and adventurous part of my journey as a developer.
+#Having founded cyberlo Digital in Mombasa, I wish to clarify any major issue other coastal developers may have had to fight for.
